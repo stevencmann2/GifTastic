8bf3eb50c5f4ca0ee4bd6f78297f65d841c6a7c2
Steven Mann <stevencmann2@gmail.com>
2019-10-27 21:46:16 -0500

    deleted unneeded button printing code, styling buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,27 @@
 //Javascript form for GifTastic Project 
-//console.log("this is linked");
 
-
-//things to fix: gifs continuouslt appending, clearing the type bar, animation and stop
+//array for initial topic buttons
 
 const topics = ["guitar", "dogs", "basketball", "baseball", "craftbeer", "coffee", "cooking", "camping", "travel", "biking", "fishing", ];
 
-// function that  creates buttons,. sources text from the array, and appends 
-function initialbuttonPrint() {
-    for (let i = 0; i < topics.length; i++) {
-        makeBtn = $("<button>");
-        //makeBtn.addClass("topics-buttons");
-        makeBtn.text(topics[i]);
-        $(".topics-div").append(makeBtn);
-    }
-};
-//prints buttons//
-initialbuttonPrint();
 
+// adds an attribute of data-keyword
 function TopicsKeywordData() {
     const topicsKeyword = $(this).attr("data-keyword");
     //alert(topicsKeyword);
 };
 
 function createButtons() {
-
     // Deleting the images prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
     $(".topics-div").empty();
     // Looping through the array of topics
     for (let i = 0; i < topics.length; i++) {
-
         // Then dynamicaly generating buttons for each topics and userInput in the array
-
         const newBtn = $("<button>");
         // Adding a class of movie-btn to our button
         newBtn.addClass("topics-buttons");
+        //adding class to style buttons using bootstrap classes
+        newBtn.addClass("btn btn-success");
         // Adding a data-attribute keyword
         newBtn.attr("data-keyword", topics[i]);
         // Providing the initial button text
@@ -53,15 +39,13 @@ $("#add-keyword").on("click", function (event) {
     const userInput = $("#topicSearchBar").val().trim();
     // Adding the topic from the textbox to our array
     topics.push(userInput);
-    console.log(userInput);
+    //console.log(userInput);
     // Calling createButtons which handles the processing of topics array
     createButtons();
 
 });
-// Function for displaying the images 
-// adding a click event listener to all elements with the class "topics-buttons"
-// We're adding the event listener to the document because it will work for dynamically generated elements
-// $(".topics-buttons").on("click") will only add listeners to elements that are on the page at that time
+ 
+//click event for appending buttons
 $(document).on("click", ".topics-buttons", TopicsKeywordData);
 // Calling the renderButtons function to display the intial buttons
 createButtons();
@@ -79,17 +63,13 @@ function userTopicsClick() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        console.log(queryURL);
+        //console.log(response);
+        //console.log(queryURL);
 
         const topicGifImages = response.data;
 
         for (i = 0; i < topicGifImages.length; i++) {
-            //
-            //animates gif URL
-            // et animatedImage = topicGifImages[i].images.fixed_height.url;
-            console.log(i);
-            //still image and text append
+            //console.log(i);
             gifDiv = $("<div>");
             const ratingText = topicGifImages[i].rating
             gifImg = $("<img>").attr("src", topicGifImages[i].images.fixed_height_still.url );
@@ -107,11 +87,14 @@ function userTopicsClick() {
 
 };
 
+//click event to change img src based on data attributes 
+//conditional to check status of img
+
 $(".images-holder").on("click", ".gifs", gifAnimate);
 
 function gifAnimate() {
     let state = $(this).attr("data-state");
-    console.log(this);
+   // console.log(this);
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animated"));
         $(this).attr("data-state", "animate");
